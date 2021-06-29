691840d3a6e8a3b34fa8a1cf286a72bf9cb89c4c
David Reinstein <davidr@profusion.com>
2021-06-29 10:13:19 -0400

    remove a few obsolete packages, some notes about `confliced'

--- functions/baseoptions.R
+++ functions/baseoptions.R
@@ -13,9 +13,12 @@ library(pacman)
 
 #p_load_gh("acoppock/attrition", "ngreifer/cobalt") # Alexander Coppock package to calculate trimming bounds. Not on CRAN
 
-p_load(arm, arsenal, bettertrace, blockTools, broom, car, citr, cobalt, codebook, coefplot, corx, data.table, dataMaid, DescTools, devtools, dplyr, estimatr, experiment, forcats, furniture, gapminder, GGally, gganimate, ggplot2, ggplot, ggpubr, ggsignif, ggtext, ggthemes, glmnet, glmnetcr, glue, gtools, gtsummary, here, Hmisc, hrbrthemes, huxtable, janitor, kableExtra, knitr, likert, lmtest, lubridate, magrittr, paramtest, pastecs, plotly, plyr, pryr, psych, pubh, purrr, pwr, randomizr, readr, readxl, recipes, reporttools, rlang, safejoin, 
-       sandwich, santoku, scales, searcher, sjlabelled, sjmisc, skimr, snakecase, statmod, summarytools, tidyverse, todor, vtable,
+p_load(arm, arsenal, bettertrace, blockTools, broom, car,  cobalt, codebook, coefplot, corx, data.table, dataMaid, DescTools, devtools, dplyr, DT,  estimatr, experiment, forcats, furniture, gapminder, GGally, gganimate, ggplot2, ggpubr, ggsignif, ggtext, ggthemes, glmnet, glmnetcr, glue, gtools, gtsummary, here, Hmisc, hrbrthemes, huxtable, janitor, kableExtra, knitr, likert, lmtest, lubridate, magrittr, paramtest, pastecs, plotly, plyr, pryr, psych, pubh, purrr, pwr, randomizr, readr, readxl, recipes, reporttools, rlang, safejoin, sandwich, santoku, scales, searcher, sjlabelled, sjmisc, skimr, snakecase, statmod, summarytools, tidyverse, todor, vtable,
       install = FALSE)
+
+print("Note we have now set install=FALSE in the p_load; thus you must install all of these packages yourself (but only once)")
+
+#TODO: I Intend to load `conflicted` here, but I would need to resolve a huge number of name conflicts
 
 
 
