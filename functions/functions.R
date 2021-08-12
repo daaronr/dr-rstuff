8c508bde6230ef6038e48e934a334a7d483ed949
os301 <os301@exeter.ac.uk>
2021-08-12 16:25:25 +0100

    Update functions.R

--- functions/functions.R
+++ functions/functions.R
@@ -691,6 +691,22 @@ boxplot_func <- function(df = ADSX, yvar = donation, treatvar = Treatment, facet
       size = 3, color = "red")
 }
 
+# https://stackoverflow.com/questions/13407236/remove-a-layer-from-a-ggplot2-chart
+# Delete layers from ggplot
+remove_geom <- function(ggplot2_object, geom_type) {
+  # Delete layers that match the requested type.
+  layers <- lapply(ggplot2_object$layers, function(x) {
+    if (class(x$geom)[1] == geom_type) {
+      NULL
+    } else {
+      x
+    }
+  })
+  # Delete the unwanted layers.
+  layers <- layers[!sapply(layers, is.null)]
+  ggplot2_object$layers <- layers
+  ggplot2_object
+}
 
 # Model building #####
 
