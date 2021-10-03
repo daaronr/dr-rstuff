1d07814fb72171eec89e9d8997d1231d522cee1e
@daaronr <daaronr@gmail.com>
2021-10-02 20:07:05 -0400

    trying the `as_image` thing

--- functions/kableextra_webshot2patch.R
+++ functions/kableextra_webshot2patch.R
@@ -1,8 +1,8 @@
-save_kable_htmlW2 <- function (x, file, bs_theme, self_contained, extra_dependencies, 
-          density, ...) 
+save_kable_htmlW2 <- function (x, file, bs_theme, self_contained, extra_dependencies,
+          density, ...)
 {
-  dependencies <- list(rmarkdown::html_dependency_jquery(), 
-                       rmarkdown::html_dependency_bootstrap(theme = bs_theme), 
+  dependencies <- list(rmarkdown::html_dependency_jquery(),
+                       rmarkdown::html_dependency_bootstrap(theme = bs_theme),
                        html_dependency_lightable(), html_dependency_kePrint())
   if (!is.null(extra_dependencies)) {
     dependencies <- append(dependencies, extra_dependencies)
@@ -11,16 +11,17 @@ save_kable_htmlW2 <- function (x, file, bs_theme, self_contained, extra_dependen
   html_table <- htmltools::HTML(as.character(x))
   html_result <- htmltools::tagList(html_header, html_table)
   if (tools::file_ext(file) %in% c("png", "jpg", "jpeg", "pdf")) {
-    file_temp_html <- tempfile(pattern = tools::file_path_sans_ext(basename(file)), 
+    file_temp_html <- tempfile(pattern = tools::file_path_sans_ext(basename(file)),
                                fileext = ".html")
     file.create(file_temp_html)
     file_temp_html <- normalizePath(file_temp_html)
     file.create(file)
     file <- normalizePath(file)
-    temp_dir <- sub(pattern = "^[\\\\/]{1,2}", replacement = "", 
+    temp_dir <- sub(pattern = "^[\\\\/]{1,2}", replacement = "",
                     tempfile(pattern = "lib", tmpdir = "", fileext = ""))
-    save_HTML(html_result, file = file_temp_html, libdir = temp_dir, 
+    kableExtra:::save_HTML(html_result, file = file_temp_html, libdir = temp_dir,
               self_contained = FALSE)
+    browser()
     result <- webshot2::webshot(file_temp_html, file, ...)
     if (is.null(result)) {
       file.remove(file)
@@ -32,7 +33,7 @@ save_kable_htmlW2 <- function (x, file, bs_theme, self_contained, extra_dependen
         message("Note that HTML color may not be displayed on PDF properly.")
       }
       file.remove(file_temp_html)
-      unlink(file.path(dirname(file_temp_html), temp_dir), 
+      unlink(file.path(dirname(file_temp_html), temp_dir),
              recursive = TRUE)
       if (requireNamespace("magick", quietly = TRUE)) {
         img_rework <- magick::image_read(file, density = density)
@@ -50,17 +51,21 @@ save_kable_htmlW2 <- function (x, file, bs_theme, self_contained, extra_dependen
     file.create(file)
     file <- normalizePath(file)
     if (self_contained) {
-      temp_dir <- sub(pattern = "^[\\\\/]{1,2}", replacement = "", 
+      temp_dir <- sub(pattern = "^[\\\\/]{1,2}", replacement = "",
                       tempfile(pattern = "lib", tmpdir = "", fileext = ""))
-      save_HTML(html_result, file = file, libdir = temp_dir, 
+      kableExtra:::save_HTML(html_result, file = file, libdir = temp_dir,
                 self_contained = TRUE)
       self_contained(file, file)
       unlink(file.path(dirname(file), temp_dir), recursive = TRUE)
     }
     else {
-      save_HTML(html_result, file = file, self_contained = FALSE)
+      kableExtra:::save_HTML(html_result, file = file, self_contained = FALSE)
     }
   }
   return(invisible(file))
 }
+
+
+unlockBinding(getNamespace("kableExtra"), sym = "save_kable_html")
+assign("save_kable_html", save_kable_htmlW2, envir = getNamespace("kableExtra"))
 
